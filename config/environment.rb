6fe17d84fd39a1216a13e0d4da133c8b7310b936
Steve Albonico <salbonico@gmail.com>
2019-02-07 22:17:28 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ Bundler.require
 
 configure :development do
 	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
+	SINATRA_ACTIVESUPPORT_WARNING=false
 end
 require_relative '../app/controllers/application_controller.rb'
 require_all 'app/models'
